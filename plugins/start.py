0e75c27afadb24c67b3fe9d3ac02b161ba38106d
Tushit15 <tushitthakur15@gmail.com>
2024-10-30 14:40:26 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,4 +1,4 @@
-#(©)CodeXBotz
+#(©)ShinChanPUX
 
 
 
@@ -88,8 +88,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("ABOUT", callback_data = "about"),
+                    InlineKeyboardButton("CLOSE", callback_data = "close")
                 ]
             ]
         )
